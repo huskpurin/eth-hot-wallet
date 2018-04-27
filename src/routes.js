393a5ac76a7f1ce38fc0c93f13bb9d3c3e025aea
abu <huskpurin@gmail.com>
2018-04-27 22:28:46 +0800

    use web3 to get block info basically

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+var router = require('express').Router();
+
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+router.get('/block/:blockNumber', (req, res) => {
+  const { blockNumber } = req.params;
+
+  if (blockNumber) {
+    web3.eth.getBlock(blockNumber, (error, result) => {
+      if (!error) {
+        res.status(200).json({
+          data: result,
+          statusCode: 200,
+          message: 'success',
+        });
+      } else {
+        res.status(500).json({
+          error,
+          statusCode: 500,
+          message: 'something wrong',
+        });
+      }
+    });
+  }
+});
+
+module.exports = router;
