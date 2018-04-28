78fe9aba5d01b179428c43b847c0695d1eda81bd
abu <huskpurin@gmail.com>
2018-04-28 22:59:33 +0800

    add middleware to check web3 provider status

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,19 @@ var Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
-router.get('/block/:blockNumber', (req, res) => {
+function web3Middleware(req, res, next) {
+  if(web3 && web3.isConnected()) {
+    next();
+  } else {
+    res.status(500).json({
+      data: null,
+      statusCode: 500,
+      message: 'Web3 provider have not been set.',
+    });
+  }
+}
+
+router.get('/block/:blockNumber', web3Middleware, (req, res) => {
   const { blockNumber } = req.params;
 
   if (blockNumber) {
