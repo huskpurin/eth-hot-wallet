522798fde0698c125dda88f753bf0302195d6539
abu <huskpurin@gmail.com>
2018-04-29 09:47:15 +0800

    add /node api by using axios to request geth rpc api

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
+var HTTP_PROVIDER = 'http://localhost:8545';
 var router = require('express').Router();
+var axios = require('axios').default;
 
 var Web3 = require('web3');
 var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+web3.setProvider(new web3.providers.HttpProvider(HTTP_PROVIDER));
 
 function web3Middleware(req, res, next) {
   if(web3 && web3.isConnected()) {
@@ -15,6 +17,38 @@ function web3Middleware(req, res, next) {
     });
   }
 }
+
+router.get('/node', web3Middleware, (req, res) => {
+  axios({
+    url: HTTP_PROVIDER,
+    method: 'post',
+    headers: {
+      'content-type': 'application/json',
+    },
+    data: {
+      jsonrpc: '2.0',
+      method: 'admin_nodeInfo',
+      id: 0,
+    },
+  })
+  .then(json => {
+    res.status(200).json({
+      data: {
+        enode: json.data.result.enode,
+        name: json.data.result.name,
+      },
+      statusCode: 200,
+      message: 'success',
+    });
+  })
+  .catch(error => {
+    res.status(500).json({
+      error,
+      statusCode: 500,
+      message: 'something wrong',
+    });
+  });
+});
 
 router.get('/block/:blockNumber', web3Middleware, (req, res) => {
   const { blockNumber } = req.params;
@@ -34,7 +68,7 @@ router.get('/block/:blockNumber', web3Middleware, (req, res) => {
           message: 'something wrong',
         });
       }
-    });
+    })
   }
 });
 
