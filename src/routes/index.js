e10a1187daedac7c78d230cefa9ac7678e465508
abu <huskpurin@gmail.com>
2018-05-07 01:16:02 +0800

    add request rate limit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,15 @@
+const RateLimit = require('express-rate-limit');
 const router = require('express').Router();
 const apiRouter = require('express').Router();
 const v1Router = require('./api/v1/index');
 
-router.use('/api', apiRouter);
+const apiLimiter = new RateLimit({
+  windowMs: 60 * 1000,
+  max: 60,
+  delayMs: 0,
+});
+
+router.use('/api', apiLimiter, apiRouter);
 
 // api router
 apiRouter.use('/v1', v1Router);
